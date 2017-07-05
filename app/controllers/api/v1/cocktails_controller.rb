23d58a1c591fc3374e2dbf89d52959a95f994c7d
linvvian <lin.vvian@gmail.com>
2017-07-05 17:00:01 -0400

    adds create

--- app/controllers/api/v1/cocktails_controller.rb
+++ app/controllers/api/v1/cocktails_controller.rb
@@ -1,29 +1,35 @@
-module Api
-  module V1
-    class CocktailsController < ApplicationController
-      def index
-        render json: Cocktail.all
+class Api::V1::CocktailsController < ApplicationController
+  def index
+    render json: Cocktail.all
+  end
+
+  def show
+    render json: Cocktail.find(params[:id])
+  end
+
+  def create
+    cocktail = Cocktail.new(name: params[:name], description: params[:description], source: params[:source], instructions:
+    params[:instructions])
+    if cocktail.save
+      params[:proportions].each do |proportion|
+        portion = Proportion.new(amount: proportion[:amount], ingredient: Ingredient.find_or_create_by(name: proportion[:ingredient]))
+        portion.save
+        cocktail.proportions << portion
       end
-
-      def show
-        render json: Cocktail.find(params[:id])
-      end
-
-      def create
-
-      end
-
-      def edit
-
-      end
-
-      def update
-
-      end
-
-      def destroy
-
-      end
+    else
+      render json: {message: cocktail.errors.full_messages, status: 500}
     end
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
   end
 end
